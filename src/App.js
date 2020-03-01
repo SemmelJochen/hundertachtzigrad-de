ff872a87f57fb462bfbdb61f0dceb299f5961763
FeedThePiano <jokup@t-online.de>
2020-03-01 13:27:15 +0100

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './ressources/img/hundertachtzigrad.png';
 import './App.css';
+import { responsiveFontSizes, createMuiTheme, ThemeProvider, Typography } from '@material-ui/core'
+import bg from './ressources/img/foto.jpg'
+const theme = responsiveFontSizes(createMuiTheme({
+  palette: {
+    // error: will use the default color
+  },
+  typography: {
+    fontFamily: [
+      'Avenir Heavy',
+      "AppleSDGothicNeo-Light",
+      "Avenir-Light",
+      "Helvetica",
+      "Verdana",
+      "Roboto"
+    ].join(','),
+  },
 
+}));
+
+
+//font: Avenir Heavy
+//logo color: #245c4e;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: "auto 100%",
+      backgroundPosition: "center center",
+      backgroundRepeat: "no-repeat",
+      width: "100%",
+      height: "100%",
+  }}>
+      <ThemeProvider theme={theme}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h4">
+            Hier kommt bald was!
+        </Typography>
+
+
+        </header>
+      </ThemeProvider>
     </div>
   );
 }
