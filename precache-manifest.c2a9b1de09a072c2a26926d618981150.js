81eba8083428deb44cb02984dd0553de05bb0599
FeedThePiano <jokup@t-online.de>
2020-03-03 14:39:09 +0100

    Updates

---
+++ precache-manifest.c2a9b1de09a072c2a26926d618981150.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92909e202e0ca9e06b964b569b19faa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1992307a3040b3d7232",
+    "url": "/static/css/main.58b4bacc.chunk.css"
+  },
+  {
+    "revision": "050fe857e421efd8655e",
+    "url": "/static/js/2.afc940f2.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.afc940f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1992307a3040b3d7232",
+    "url": "/static/js/main.94c5460c.chunk.js"
+  },
+  {
+    "revision": "f8bc525a3cf277b8f2ea",
+    "url": "/static/js/runtime-main.8d70cad7.js"
+  },
+  {
+    "revision": "f51a88468658d467d358f811971fdea0",
+    "url": "/static/media/foto.f51a8846.jpg"
+  },
+  {
+    "revision": "88899a6456081edb8edff10196200cb2",
+    "url": "/static/media/hundertachtzigrad.88899a64.png"
+  }
+]);
